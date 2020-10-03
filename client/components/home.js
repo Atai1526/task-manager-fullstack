7c13feac5e5f303217baa3e1f6317dae525dcc72
Berdaliev Atai <bboy10@list.ru>
2020-10-03 18:06:53 +0300

    updated

--- client/components/home.js
+++ client/components/home.js
@@ -51,7 +51,7 @@ const Home = () => {
 
   return (
     <div>
-      <div className="flex items-center justify-center h-screen">
+        <div className="flex items-center justify-center h-screen">
         <div className="bg-yellow-400  font-bold rounded-lg border shadow-lg p-10">
           <Route
             exact
